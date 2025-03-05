23c46e28f567a1dba69547ede077ff93db76e048
exsize <exsize426@mail.ru>
2025-03-05 21:04:22 +0300

    create new branch object

--- index.js
+++ index.js
@@ -1,37 +1,9 @@
-// Пример использовании цикла
-let age ;
-
-for(age = 2; age <=10; age++) {
-    (age % 2 == 0) ? console.log(age) : false
+const objectA = {
+    a: 10,
+    b: true,
 }
 
-for(age = 2; age <=10; age++) {
-    if (age % 2 == 0) {
-        console.log(age)
-    }
-}
-//Пример использовании функции:
-function checkAge(age) {
-   return (age > 18) ?  true :  confirm('Родители разрешили?');
-  }
-
-  let res = checkAge(10);
-  console.log(res)
-
-//------------- Пример 2.--------------
-
-let min = (a,b) => {
-  return (a > b) ? b : a;
-}
-
-let results = min(2, 5);
-  console.log(results)
-
-//------------- Пример 3.--------------
-
-let pow = (x,n) => {
-    return x ** n
-}
-let result = pow(1, 100);
-  console.log(result)
-
+const copy0fA = objectA
+   copy0fA.a = 20
+   objectA['isCar'] = false;
+  console.log(objectA)
