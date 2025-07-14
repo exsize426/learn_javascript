672aa053ce5e974cd660cb861c045de22035c42a
exsize <exsize426@mail.ru>
2025-07-14 23:34:52 +0300

    feature(*): validation form

--- Components/Weather.jsx
+++ Components/Weather.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function WeatherApp() {
   const [city, setCity] = useState('');
   const [weatherRostov, setWeatherRostov] = useState(null);
   const [weatherAzov, setWeatherAzov] = useState(null);
+  const [isValid, setIsValid] = useState(false)
+  const [example, setExample] = useState([weatherAzov,weatherRostov])
 
   const getWeather = async () => {
     const API_KEY = 'ed5c2c5ad0514524890175244250907'; // получите на openweathermap.org
@@ -16,49 +18,73 @@ export default function WeatherApp() {
     const dataAzov = await responseAzov.json();
     const dataRostov = await responseRostov.json();
 
+
     setWeatherRostov(dataRostov);
     setWeatherAzov(dataAzov);
+
   };
 
-  
+  // const exapmleTown = (value) => {
+  //     setExample([...example].map((item) => item == value))
+  //     console.log(example)
+  // }
 
+
+
+
+    // useEffect(() => {
+    //     if (city) {
+    //       getWeather();
+    //     }
+    //   }, [city]);
+
+  const validateInput  = (value) => {
+    const word = value[0].toUpperCase() + value.slice(1).toLowerCase();
+    const arrayTown = ["Azov","Rostov","Азов","Ростов" ]
+    
+    return arrayTown.includes(word)
+  }
+ 
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setWeatherRostov(null);
+    setWeatherAzov(null);
+    setCity(value)
+    setIsValid(validateInput(value))
+    setExample(value)
+  }
+  
+  const handleSubmit = (event) => {
+       event.preventDefault();
+
+  }
+  
   return (
-    <>
-    <div style={{ padding: '20px', fontFamily: 'Arial' }}>
+    <form onSubmit={handleSubmit}>
+    
       <input
         value={city}
-        onChange={(e) => setCity(e.target.value)}
+        onChange={handleChange}
         placeholder="Введите город"
       />
-      <button onClick={getWeather}>Узнать погоду</button>
       
-      {weatherRostov && ( // тоже самое weatherRostov ? условие, если weatherRostov null,underfined, false - то реакт проигнорирует этот блок
-        <div style={{ marginTop: '20px' }}>
-          <h3>Погода города {weatherRostov.location.name}</h3>
-          <p>Температура: {weatherRostov.current.temp_c}°C</p> 
-          
-          {/* <p>{weather.weather[0].description}</p> */}
-        </div>
-      )}
-    </div>
+      <button type="submit" onClick={getWeather}>Узнать погоду</button>
+      
+        {weatherRostov && isValid && ( // тоже самое weatherRostov ? условие, если weatherRostov null,underfined, false - то реакт проигнорирует этот блок
+          <div style={{ marginTop: '20px' }}>
+            <h3>Погода города {weatherRostov?.location.name}</h3>
+            <p>Температура: {weatherRostov?.current.temp_c}°C</p> 
+          </div>
+        )}
 
-      <div style={{ padding: '20px', fontFamily: 'Arial' }}>
-      {/* <input
-        value={city}
-        onChange={(e) => setCity(e.target.value)}
-        placeholder="Введите город"
-      />
-      <button onClick={setWeatherAzov}>Узнать погоду</button> */}
-      
-      {weatherAzov && (
-        <div style={{ marginTop: '20px' }}>
-          <h3>Погода города {weatherAzov.location.name}</h3>
-          <p>Температура: {weatherAzov.current.temp_c}°C</p> 
-          {/* Math.round(weather.main.temp_c) */}
-          {/* <p>{weather.weather[0].description}</p> */}
-        </div>
-      )}
-    </div>
-    </>
+        {weatherAzov && isValid && (
+          <div style={{ marginTop: '20px' }}>
+            <h3>Погода города {weatherAzov?.location.name}</h3>
+            <p>Температура: {weatherAzov?.current.temp_c}°C</p> 
+          </div>
+        )}
+
+    </form>
   );
 }
+ 
